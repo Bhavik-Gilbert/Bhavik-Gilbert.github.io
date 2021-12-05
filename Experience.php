9349ce60abb549cb4d4ed789e6d958cca344ad15
Bhavik-Gilbert <bhaviklob@gmail.com>
2021-12-05 01:51:12 +0000

    changes

--- Experience.php
+++ Experience.php
@@ -4,13 +4,121 @@
 </head>
 
 <?php
-include "Entity/menu.php"
+include "Entity/menu.html"
 ?>
 
 <body>
 <div align="center">
+<h1>Experiences</h1>
+<h3>Work Experience</h3>
+<div class=blockabout align="center">
+<h4 > Co-Founder and Co-Chief Executive Officer</h4>
+<h5 >StemItup<br>08/2020</h5>
+<h6><br><a href=https://www.stemitup.org/ class=btn>View Website</a><br></h6>
+ Aiding in the creation of a startup that aims to spark an interest in science within the younger generation. 
+ Responsibilities held were varied due to the small nature of the company and the seniority of the position, 
+ with the main responsibilities ranging from website management and development, project management, general 
+ management to overall business development with additional tasks on the side. Furthermore, involves the tutoring 
+ of children and any tutors that got involved.
+</div>
+<br>
+<div class=blockabout align="center">
+<h4 > Technology Virtual Work Experience</h4>
+<h5 >Springpod<br>04/2021-04/2021</h5>
+<h6><br><a href=https://www.linkedin.com/in/bhavik-gilbert-2349631a9/detail/treasury/position:1766009453/?entityUrn=urn%3Ali%3Afsd_profileTreasuryMedia%3A(ACoAADC6t4gB9zkJSNbpb2fGu0zKySRQv96N8cQ%2C1618440969668)&section=position%3A1766009453&treasuryCount=2
+ class=btn>View Certification</a><br></h6>
+ <h6><br><a href=https://www.springpod.co.uk/experience/technology-work-experience class=btn>View Work</a><br></h6>
+ - A sound understanding of the inner-workings and concepts related to technology, and the ability to identify the skills needed to pursue a career in this sector.
+- The ability to successfully apply knowledge of the technology sector by completing quizzes, interactive exercises and activities.
+- Meaningful interaction with industry professionals through live webinars.
+- A better understanding of the career pathways within technology.
+- Knowledge of key employability and transferable skills, as well as a sound awareness of employer expectations.”
+</div>
+<br>
+<div class=blockabout align="center">
+<h4> Career's Camp</h4>
+<h5>Digital Summer Camp<br>
+Open Awards Entry Level Award in Employability and Professional Development(Entry 3)(RQF)<br>11/2019-12/2019</h5>
+<h6><br><a href=https://www.linkedin.com/in/bhavik-gilbert-2349631a9/detail/treasury/position:1744523868/?entityUrn=urn%3Ali%3Afsd_profileTreasuryMedia%3A(ACoAADC6t4gB9zkJSNbpb2fGu0zKySRQv96N8cQ%2C1618441276401)&section=position%3A1744523868&treasuryCount=1
+ class=btn>View Certification</a><br></h6>
+Competition for people from 14-28 supported by Waltham Forest council. Qualifiers were tasked to design a 
+product to help improve safety in the borough from Waltham Forest. Within my group of 4, I was product and 
+project manager, where our pitch and design of an app idea placed us 2nd. It helped not only with team building 
+and leadership but also became a great opportunity to learn how to network with people from a business background.
+</div>
+<br>
+<h3>Education</h3>
+<div class=blockabout align="center">
+<h4>A-Levels</h4>
+<br>
+Mathematics : A*
+Economics : A*
+Computer Science : A
+Physics : A
+</div>
+<br>
+<div class=blockabout align="center">
+<h4>GCSES</h4>
+<br>
+Geography : 9/A*
+Mathematics : 8/A*
+Biology : 8/A*
+Chemistry : 8/A*
+English Literature : 8/A*
+Computer Science : 7/A
+<br>
+Business : 7/A
+Physics : 7/A
+Religion : 7/A
+Further Mathematics : B
+English Language : 6/B
+Spanish : 4/C 
+</div>
+
+<br>
+<h3>Online Courses</h3>
+<div class=blockabout align="center">
+<h4> UNIQ Digital - Oxford</h4>
+<h5> Online Course - RStudio<br>03/2020-11/2020</h5>
+<h6><br><a href=https://digital.uniq.ox.ac.uk class=btn>View Work</a><br></h6>
+An online programme run by Oxford university connecting current students to prospective students. Its focus was on giving us resources 
+to be better prepared for the university application process through tasks such as research projects and essay writing.
+An optional R-Studio programming course was provided, which gave an in-depth explanation and understanding of the language with a 
+programming test at the end to gauge what we learnt at the end of the course, where I achieved 95/100.
+</div>
+<br>
+<div class=blockabout align="center">
+<h4> OpenLearn University</h4>
+<h5> OpenLearn CourseDesign thinking</h5>
+<h5> 04/2020-05/2020</h5>
+<h6><br><a href=https://www.linkedin.com/in/bhavik-gilbert-2349631a9/detail/treasury/position:1744524136/?entityUrn=urn%3Ali%3Afsd_profileTreasuryMedia%3A(ACoAADC6t4gB9zkJSNbpb2fGu0zKySRQv96N8cQ%2C1614473782212)&section=position%3A1744524136&treasuryCount=1
+ class=btn>View Certification</a><br></h6>
+This OpenLearn course is an adapted extract from the Open University course U101Design thinking: creativity for the 21st century.
+<br><br><br>
+<h5> OpenLearn Course An introduction to software development</h5>
+<h5> 04/2020-05/2020</h5>
+<h6><br><a href=https://www.linkedin.com/in/bhavik-gilbert-2349631a9/detail/treasury/position:1744525048/?entityUrn=urn%3Ali%3Afsd_profileTreasuryMedia%3A(ACoAADC6t4gB9zkJSNbpb2fGu0zKySRQv96N8cQ%2C1614473276992)&section=position%3A1744525048&treasuryCount=1
+ class=btn>View Certification</a><br></h6>
+This OpenLearn course is an adapted extract from the Open University course M813 Software development.
+<br><br><br>
+<h5>OpenLearn Course Technological innovation: a resource-based view</h5>
+<h5> 04/2020-05/2020</h5>
+<h6><br> <a href=https://www.linkedin.com/in/bhavik-gilbert-2349631a9/detail/treasury/position:1744525108/?entityUrn=urn%3Ali%3Afsd_profileTreasuryMedia%3A(ACoAADC6t4gB9zkJSNbpb2fGu0zKySRQv96N8cQ%2C1614473926823)&section=position%3A1744525108&treasuryCount=1
+ class=btn>View Certification</a><br></h6>
+This OpenLearn course is an adapted extract from the Open University course T849 Strategic capabilities for technological innovation.
+<br><br><br>
+<h5>OpenLearn Course Computers and computer systems</h5>
+<h5> 03/2020-05/2020</h5>
+<h6><br><a href=https://www.linkedin.com/in/bhavik-gilbert-2349631a9/detail/treasury/position:1744522680/?entityUrn=urn%3Ali%3Afsd_profileTreasuryMedia%3A(ACoAADC6t4gB9zkJSNbpb2fGu0zKySRQv96N8cQ%2C1614473496374)&section=position%3A1744522680&treasuryCount=1
+ class=btn>View Certification</a><br></h6>
+This OpenLearn course provides a sample of level 2 study in Computing & IT.
+</div>
 </div>
 
 <br><br>
-</div>
+
+
+<?php
+include "Entity/footer.html"
+?>
 </body>
---
+++ index.php
@@ -0,0 +1,72 @@
+<head>
+    <link rel="stylesheet" type="text/css" href="CSS/Base.css">
+    <title>Landing Page</title>
+</head>
+
+<?php
+include "Entity/menu.html"
+?>
+
+<body>
+<div align="center">
+<h1>Welcome to my Portfolio</h1>
+<h6>Go to the advertised pages for more information</h6>
+
+<h3>Current Projects</h3>
+<div class="info"> 
+<h4>Photography Website</h4>
+<h5>Coursework</h5>
+<h6><a href=http://bhavik-portfolio.epizy.com/Websites/Coursework%20-%20Photography/Home.php class=btn>
+View Work</a><br></h6>
+<img class="headpic" src="CSS/Images/Titles/Coursework.PNG"></div>
+
+<br><br>
+
+<h3>Past Project Highlights</h3>
+<div class="info"> 
+<h4>UNIQ Digital - Oxford</h4>
+<h5>Online Course - RStudio</h5>
+<h6><br><br><br> <a href=https://digital.uniq.ox.ac.uk class=btn>View Work</a></h6>
+</div>
+<div class="info"> 
+<h4>QMUL Python Exoplanet Hunting Programme</h4>
+<h5>Kepler Data Analysis & Graphical Manipulation</h5>
+    <img class="headpic" src="CSS/Images/Titles/Exoplanet.PNG">
+</div>
+<br><br>
+
+<h3>Work Experience</h3>
+<div class="info"> 
+<h4>StemItup</h4>
+<h5>Co-Founder and Co-Chief Executive Officer</h5>
+<h6><br><a href=https://www.stemitup.org/ class=btn>View Website</a><br></h6>
+</div>
+<div class="info"> 
+<h4>Springpod</h4>
+<h5>Technology Virtual Work Experience</h5>
+<h6><a href=https://www.linkedin.com/in/bhavik-gilbert-2349631a9/detail/treasury/position:1766009453/?entityUrn=urn%3Ali%3Afsd_profileTreasuryMedia%3A(ACoAADC6t4gB9zkJSNbpb2fGu0zKySRQv96N8cQ%2C1618440969668)&section=position%3A1766009453&treasuryCount=2
+ class=btn>View Certification</a></h6>
+ <br>
+<h6><a href=https://www.springpod.co.uk/experience/technology-work-experience class=btn>View Work</a></h6>
+</div>
+<br><br>
+
+<h3>Education</h3>
+<div class="info">
+    <h4>A Levels</h4>
+    <h5>Computer Science - A<br>Mathematics - A*<br>Physics - A<br>Economics - A*<br>
+    </h5>
+</div>
+
+<div class="info">
+    <h4>GCSEs</h4>
+    <h5>Computer Science-7 ; Physics - 7<br>Mathematics-8 ; Business - 7<br>Further Maths - B
+    <br>12 GCSEs total<br> 9 at 7/A equivalent or above</h5>
+</div>
+
+<br><br>
+</div>
+<?php
+include "Entity/footer.html"
+?>
+</body>
