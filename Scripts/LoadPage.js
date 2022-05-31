223ee4c8279289c41881e74ebca08c637e9e9d7f
Bhavik-Gilbert <83555685+Bhavik-Gilbert@users.noreply.github.com>
2022-05-31 17:54:23 +0100

    name capital effect

--- Scripts/LoadPage.js
+++ Scripts/LoadPage.js
@@ -40,5 +40,44 @@ function handheldcheck() {
     return check;
 }
 
+function setuph1main(text) {
+    for (let i = 0; i < text.length; i++) {
+        var letter = document.createElement("div");
+        if (handheldcheck() && text[i] == " ") {
+            letter.innerHTML = "_";
+            letter.id = "_" + i;
+            letter.onclick = function () { toggleh1main("_"+ i); };
+        }
+        else {
+            letter.innerHTML = text[i];
+            letter.id = text[i] + i;
+            letter.onclick = function () { toggleh1main(text[i] + i); };
+        }
+        letter.className = "h1main";
+        let parent = document.getElementsByClassName("start")[0];
+        parent.appendChild(letter);
+    }
+}
+
+function toggleh1main(id) {
+    let letter = document.getElementById(id);
+    if (typeof (letter) != 'undefined' && letter != null) {
+        classname = letter.className;
+        letter.innerHTML = reversecapitalize(letter.innerHTML);
+        if (classname == "h1main") {
+            letter.className = "h1hold";
+            return;
+        }
+        letter.className = "h1main";
+    }
+}
+
+function reversecapitalize(letter) {
+    if(letter == letter.toUpperCase()) {
+        return letter.toLowerCase();
+    }
+
+    return letter.toUpperCase();
+}
 // smooth scrolling
 $("html, body").animate({ scrollTop: $(".chat-msg-list") }, 1000);
