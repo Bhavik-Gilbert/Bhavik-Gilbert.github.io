5568c09f21f00d3e71bbd02e00071d0a3d72b9c0
bhavik gilbert <bhaviklob@gmail.com>
2023-08-30 14:39:35 +0100

    Experience highlights no popup

--- new_site/portfolio/src/pages/Experience/components/subpages/ExperienceHighlights.tsx
+++ new_site/portfolio/src/pages/Experience/components/subpages/ExperienceHighlights.tsx
@@ -6,7 +6,7 @@ function ExperienceHighlights() {
   const title = "Highlights".split("");
   
   return (
-    <div>
+    <>
       <div className="marginTop marginBottom">
         {title.map((titleChar: string, titleCharIndex: number) => (
             <div className="h3 byLetter marginBottom" id={"pagesTitle" + titleCharIndex + titleChar} key={"pagesTitle" + titleCharIndex + titleChar} 
@@ -20,9 +20,41 @@ function ExperienceHighlights() {
       </div>
 
       <div>
-        Experience
+        <div className='highlightBox marginBottom'>
+          <img className='highlightBoxImage' src="./images/companyLogos/JPMorganChase.jpg" alt="pdf logo"></img>
+
+          <div className='highlightBoxText'>
+            <h5>Software Engineer Analyst Intern</h5>
+            <br/>
+            <h6>J.P Morgan Chase</h6>
+            <h6>Jun 2023 - Aug 2023</h6>
+          </div>
+            
+        </div>
+        
+        <div className='highlightBox marginBottom'>
+          <img className='highlightBoxImage' src="./images/companyLogos/LeytonXIFormCollege.png" alt="pdf logo"></img>
+
+          <div className='highlightBoxText'>
+            <h5>Computer Science Student Mentor</h5>
+            <br/>
+            <h6>Leyton Sixth Form College</h6>
+            <h6>Sep 2021 - Oct 2022</h6>
+          </div>
+        </div>
+
+        <div className='highlightBox marginBottom'>
+          <img className='highlightBoxImage' src="./images/companyLogos/Funtech.png" alt="pdf logo"></img>
+
+          <div className='highlightBoxText'>
+            <h5>Location Camp Manger</h5>
+            <br/>
+            <h6>FunTech</h6>
+            <h6>Jul 2022 - Aug 2022</h6>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
