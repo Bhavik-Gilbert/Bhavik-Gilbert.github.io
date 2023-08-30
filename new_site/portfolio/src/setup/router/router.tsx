1c2c6e1743cb938f307a2f4a22bcf2cff150e338
bhavik gilbert <bhaviklob@gmail.com>
2023-08-30 15:47:48 +0100

    router class page

--- new_site/portfolio/src/setup/router/router.tsx
+++ new_site/portfolio/src/setup/router/router.tsx
@@ -5,14 +5,16 @@ import * as Pages from './pages';
 
 const Router = () => {
   return (
-    <Routes> {/* The Switch decides which component to show based on the current URL.*/}
-      <Route path={Pages.pageLinks.Home} element={<Pages.Home/>} />
-      <Route path={Pages.pageLinks.Projects} element={<Pages.Projects/>} />
-      <Route path={Pages.pageLinks.Experience} element={<Pages.Experience/>} />
-      <Route path={Pages.pageLinks.Education} element={<Pages.Education/>} />
-      <Route path={Pages.pageLinks.Settings} element={<Pages.Settings/>} />
-      <Route path={Pages.pageLinks.Contact} element={<Pages.Contact/>} />
-    </Routes>
+    <div className="page">
+      <Routes> {/* The Switch decides which component to show based on the current URL.*/}
+        <Route path={Pages.pageLinks.Home} element={<Pages.Home/>} />
+        <Route path={Pages.pageLinks.Projects} element={<Pages.Projects/>} />
+        <Route path={Pages.pageLinks.Experience} element={<Pages.Experience/>} />
+        <Route path={Pages.pageLinks.Education} element={<Pages.Education/>} />
+        <Route path={Pages.pageLinks.Settings} element={<Pages.Settings/>} />
+        <Route path={Pages.pageLinks.Contact} element={<Pages.Contact/>} />
+      </Routes>
+    </div>
   );
 }
 
