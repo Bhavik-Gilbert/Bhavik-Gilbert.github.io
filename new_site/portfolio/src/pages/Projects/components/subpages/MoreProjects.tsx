6e8de96541d6044cf3d94f154042f93ba894e6bb
bhavik gilbert <bhaviklob@gmail.com>
2023-08-31 12:33:00 +0100

    More projects images

--- new_site/portfolio/src/pages/Projects/components/subpages/MoreProjects.tsx
+++ new_site/portfolio/src/pages/Projects/components/subpages/MoreProjects.tsx
@@ -21,7 +21,7 @@ function MoreProjects() {
 
       <div>
         <div className="card long m-3">
-            <img className="header" src="./images/companyLogos/Oxford.png" alt="Oxford University"></img>
+            <img className="header" src="./images/projects/Penguin Empire/PE1.png" alt="Penguin Empire"></img>
             <h5>Penguin Empire</h5>
             <h6>Social Media Application</h6>
             <br/>
@@ -35,7 +35,7 @@ function MoreProjects() {
         </div>
 
         <div className="card long m-3">
-            <img className="header" src="./images/companyLogos/Oxford.png" alt="Oxford University"></img>
+            <img className="header" src="./images/projects/Minigame Suite/MGS1.png" alt="Minigame Suite"></img>
             <h5>Minigame Suite</h5>
             <br/>
             <h6>Java GUI</h6>
@@ -46,7 +46,7 @@ function MoreProjects() {
         </div>
         
         <div className="card long m-3">
-            <img className="header" src="./images/companyLogos/Oxford.png" alt="Oxford University"></img>
+            <img className="header" src="./images/projects/Ticketing System/TS1.png" alt="Ticketing System"></img>
             <h5>Ticketing System</h5>
             <br/>
             <h6>Flask Project</h6>
@@ -60,7 +60,7 @@ function MoreProjects() {
         </div>
 
         <div className="card long m-3">
-            <img className="header" src="./images/companyLogos/Oxford.png" alt="Oxford University"></img>
+            <img className="header" src="./images/projects/Music School Management System/MSMS1.png" alt="Music School Management System"></img>
             <h5>Music School Management System</h5>
             <br/>
             <h6>Django Project</h6>
@@ -73,7 +73,7 @@ function MoreProjects() {
         </div>
 
         <div className="card long m-3">
-            <img className="header" src="./images/companyLogos/Oxford.png" alt="Oxford University"></img>
+            <img className="header" src="./images/projects/Typing Speed Test/TST1.png" alt="Typing Speed Test"></img>
             <h5>Typing Speed Test</h5>
             <br/>
             <h6>React Project</h6>
@@ -85,8 +85,8 @@ function MoreProjects() {
         </div>
 
         <div className="card long m-3">
-            <img className="header" src="./images/companyLogos/Oxford.png" alt="Oxford University"></img>
-            <h5>Property Viewer Application</h5>
+            <img className="header" src="./images/projects/Property Viewer/PV1.png" alt="Property Viewer"></img>
+            <h5>Property Viewer</h5>
             <br/>
             <h6>Java GUI</h6>
             <h6>4 Contributors</h6>
@@ -96,7 +96,7 @@ function MoreProjects() {
         </div>
 
         <div className="card long m-3">
-            <img className="header" src="./images/companyLogos/Oxford.png" alt="Oxford University"></img>
+            <img className="header" src="./images/projects/Ecosystem/ES1.png" alt="Ecosystem Simulator"></img>
             <h5>Ecosystem Simulator</h5>
             <br/>
             <h6>Java GUI</h6>
@@ -107,7 +107,7 @@ function MoreProjects() {
         </div>
 
         <div className="card long m-3">
-            <img className="header" src="./images/companyLogos/Oxford.png" alt="Oxford University"></img>
+            <img className="header" src="./images/projects/Photography Booking System/BS1.png" alt="Photography Booking System"></img>
             <h5>Photography Booking & Management System</h5>
             <br/>
             <h6>Web Development</h6>
@@ -120,7 +120,7 @@ function MoreProjects() {
         </div>
 
         <div className="card long m-3">
-            <img className="header" src="./images/companyLogos/Oxford.png" alt="Oxford University"></img>
+            <img className="header" src="./images/projects/Projects.png" alt="Projects"></img>
             <h5>AI Chatbot</h5>
             <br/>
             <h6>Image & Text Data Analysis</h6>
@@ -130,7 +130,7 @@ function MoreProjects() {
         </div>
 
         <div className="card long m-3">
-            <img className="header" src="./images/companyLogos/Oxford.png" alt="Oxford University"></img>
+            <img className="header" src="./images/projects/Projects.png" alt="Projects"></img>
             <h5>Pathfinder</h5>
             <br/>
             <h6>A* Pathfinder</h6>
