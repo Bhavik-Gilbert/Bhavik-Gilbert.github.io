8c7234c18673870f61eceabd6f815843b20a8775
bhavik gilbert <bhaviklob@gmail.com>
2023-09-16 22:32:59 +0100

    Feedback Overlay Styling

--- src/pages/Contact/components/infoOverlays/FormOutputInfo.tsx
+++ src/pages/Contact/components/infoOverlays/FormOutputInfo.tsx
@@ -4,17 +4,17 @@ import  { closeElements } from '../../../../setup/components/displayControls';
 
 function FormInfo() {
   return (
-    <div className="overlayInfo" id="info">
+    <div className="overlayInfo outputInfoBackground" id="info">
       <div className="topRight">
         <div className="file button marginLeft" onClick={(e) => closeElements(["infoOverlay", "info"])}>
           &#10005;
         </div>
       </div>
 
-      <div className="outputInfo">
-        <div className="outputInfoMessage">
+      <div className="outputInfoBox">
+        <div className="outputInfo">
           <div className='outputInfoSymbol' id="outputSymbol"></div>
-          <h4 id="outputMessage"></h4>
+          <h4 className="outputInfoMessage" id="outputMessage"></h4>
         </div>
       </div>
     </div>
